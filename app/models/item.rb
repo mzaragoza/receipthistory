8154016dde9e0c9d12b9658941ba5ea463e41069
Moises Zaragoza <mZaragoza@circletechfl.com>
2014-09-05 22:40:49 -0400

    update profit

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ActiveRecord::Base
 
   def profit
-    self.ammount_sold - (self.ammount_purchased + self.shipping_cost)
+    self.ammount_sold - (self.ammount_purchased + self.shipping_cost + self.site_selling_fee + self.paypal_selling_fee)
   end
 end
 
